7e8bf1c0a9d801ffb69afed06dc2646be04724c9
차주석 <juseok@chajuseog-ui-MacBookPro.local>
2022-01-01 18:08:44 +0100

    A tag

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function renderLeads() {
     let listItems = ""
     for (var i = 0; i < myLead.length; i++) {
         // ulEl.innerHTML +=  "<li>" + myLead[i] + "</li>"
-        listItems += "<li>" + myLead[i] + "</li>"
+        listItems += "<li>" + `<a href=${myLead[i]} target="blank">`+ myLead[i] +"</a>"+ "</li>"
     };
     
     ulEl.innerHTML = listItems
