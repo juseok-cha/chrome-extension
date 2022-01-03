be4a4c48930a320d3d2f1af9cae31ce5e1c24954
juseok-cha <annoate@gmail.com>
2022-01-03 09:50:15 +0100

    delete button

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
+let myLead = [];
 const inputBtn = document.getElementById("input-btn");
-const myLead = [];
+const deleteBtn = document.getElementById("delete-btn");
 const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLead"));
 
+if (leadsFromLocalStorage) {
+    myLead = leadsFromLocalStorage
+    console.log(leadsFromLocalStorage)
+    render(myLead);
+}
 
-inputBtn.addEventListener("click", function() {
-    if (inputEl.value === "") {
-        alert("Do not enter empty!")
-    } else {
-        myLead.push(inputEl.value)
-        renderLeads();
-    }
-    inputEl.value=null;
-});
-
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (var i = 0; i < myLead.length; i++) {
-        // ulEl.innerHTML +=  "<li>" + myLead[i] + "</li>"
-        listItems += 
+    for (var i = 0; i < leads.length; i++) {
+        if (leads[i] != "") {
+            listItems += 
                     `<li>
-                        <a href=${myLead[i]} target="blank">
-                            ${myLead[i]}
+                        <a href=${leads[i]} target="blank">
+                            ${leads[i]}
                         </a>
                     </li>`
+        }
+        // ulEl.innerHTML +=  "<li>" + myLead[i] + "</li>"
     };
     
     ulEl.innerHTML = listItems
 }
+
+inputBtn.addEventListener("click", function() {
+    myLead.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLead", JSON.stringify(myLead))
+    console.log("add");
+});
+
+deleteBtn.addEventListener("dblclick",function() {
+    localStorage.clear();
+    myLead = [];
+    render(myLead);
+    console.log("deleted")
+} )
